264aa98ec1bf0a0055dcb22e4a473acd9aa51a23
unknown <ajameslusk@gmail.com>
2018-07-19 22:12:33 -0400

    added GET user by id and username endpoints

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 
 const User = require('../models/user');
 
+// Should be protected or removed in production
+// GET all users
 router.get('/', (req, res, next) => {
   User.find()
     .sort('name')
@@ -13,6 +15,28 @@ router.get('/', (req, res, next) => {
       res.json(results);
     })
     .catch(err => next(err));
+});
+
+// GET user by username
+router.get('/username/:username', (req, res, next) => {
+  User.find({username: req.params.username})
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// GET user by MongoDB _id
+router.get('/id/:id', (req, res, next) => {
+  User.find({_id: req.parmas.id})
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
