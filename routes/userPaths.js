93252d63fb9ca387e24af3b1795b9b1ef2c14d80
unknown <ajameslusk@gmail.com>
2018-07-25 15:33:21 -0400

    updated PUT api/userpaths/save for new DB

--- routes/userPaths.js
+++ routes/userPaths.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const passport = require('passport');
+const ObjectId = require('mongoose').Types.ObjectId;
 
 const UserPaths = require('../models/userPath');
 const Path = require('../models/path');
@@ -10,7 +11,6 @@ const User = require('../models/user');
 
 router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
 
-/* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
   const userId = req.user.id;
   UserPaths.find({ userId })
@@ -24,28 +24,64 @@ router.put('/save', (req, res, next) => {
   const userId = req.user.id;
   const { pathId } = req.body;
 
-  UserPaths.find({ userId })
-    .then(user => {
-      Path.findById(pathId, (err, newPath) => {
+  if(!ObjectId.isValid(pathId) || !ObjectId.isValid(userId)){
+    const err = new Error('Provided path Id or user Id is not a valid ObjectId');
+    err.status = 400;
+    return next(err);
+  }
+
+  UserPaths.findOne({userId})
+    .then(userpath => {
+      Path.findById(pathId, (err, path) => {
+
+        // Add err and empty return handling
+
         let duplicate = false;
-        for(let i = 0; i < user[0].savedPaths.length; i++){
-          if(user[0].savedPaths[i]._id.toString() === pathId){
+        for(let i = 0; i < userpath.savedPaths.length; i++){
+          if(userpath.savedPaths[i]._id.toString() === pathId){
             duplicate = true;
             break;
           }
         }
         if(!duplicate){
-          user[0].savedPaths.push(newPath);
-          user[0].save();
+          userpath.savedPaths.push(pathId);
+          userpath.save();
         }
+
+        // Add validation path is not current or completed
+
       });
-      return user[0];
+      return;
     })
-    .then(result => {
-      console.log(result);
-      res.json(result);
+    .then(() => {
+      res.status(202).send();
     })
-    .catch(err => next(err));
+    .catch(err => {
+      next(err);
+    });
+
+  // UserPaths.find({ userId })
+  //   .then(user => {
+  //     Path.findById(pathId, (err, newPath) => {
+  //       let duplicate = false;
+  //       for(let i = 0; i < user[0].savedPaths.length; i++){
+  //         if(user[0].savedPaths[i]._id.toString() === pathId){
+  //           duplicate = true;
+  //           break;
+  //         }
+  //       }
+  //       if(!duplicate){
+  //         user[0].savedPaths.push(newPath);
+  //         user[0].save();
+  //       }
+  //     });
+  //     return user[0];
+  //   })
+  //   .then(result => {
+  //     console.log(result);
+  //     res.json(result);
+  //   })
+  //   .catch(err => next(err));
 });
 
 router.put('/display', (req, res, next) => {
